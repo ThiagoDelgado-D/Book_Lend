f6993376fda805f0ebbd39ddaf6ede2d8d95b9fb
Thiago <thiagodelgado.dev@gmail.com>
2025-07-28 21:50:09 -0300

    feat: update CryptoService interface to include generateRandomToken method

--- domain/src/services/crypto-service.ts
+++ domain/src/services/crypto-service.ts
@@ -4,4 +4,5 @@ export interface CryptoService {
   hashPassword(password: string): Promise<string>;
   comparePassword(password: string, hashPass: string): Promise<boolean>;
   generateUUID(): Promise<UUID>;
+  generateRandomToken(): Promise<string>;
 }
