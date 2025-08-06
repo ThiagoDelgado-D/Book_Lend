776e349bd6a3fe0ff4bbd45b61673665b16486c0
Thiago <thiagodelgado.dev@gmail.com>
2025-08-06 18:55:51 -0300

    chore: duplicate repositories are removed

--- apps/api/src/repositories/index.ts
+++
@@ -1,3 +0,0 @@
-export { UserRepository } from './user.repository.js';
-export { AuthorRepository } from './author.repository.js';
-export { BookRepository, type BookStatistics } from './book.repository.js';
