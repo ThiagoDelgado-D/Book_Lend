e639a5ebd75ad3a747efb2faa58b8c7344b957b7
Thiago <thiagodelgado.dev@gmail.com>
2025-09-01 22:04:00 -0300

    feat: add BookResponse and BooksListResponse interfaces for book API responses

--- packages/api-types/src/author/author.ts
+++
@@ -1,9 +0,0 @@
-import { Author } from 'app-domain';
-import { ApiResponse } from '../response';
-
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface AuthorResponse extends ApiResponse<Author> {}
-
-export interface AuthorsListResponse extends ApiResponse<Author[]> {
-  total?: number;
-}
