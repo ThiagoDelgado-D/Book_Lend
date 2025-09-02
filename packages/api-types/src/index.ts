8aca4afe99e95a7f78acb060ad7d483c6ba08cdd
Thiago <thiagodelgado.dev@gmail.com>
2025-09-01 22:05:21 -0300

    chore: change name and add exports

--- packages/api-types/src/index.ts
+++ packages/api-types/src/index.ts
@@ -1,5 +1,7 @@
 export * from './response';
 export * from './request';
+export * from './books';
+export * from './author';
 export * from './auth/auth';
 export * from './errors/error';
 export * from './factory';
